8ed145b3ddc771302b2b23cb5ceeaac7cbd4892a
mrhouzlane <mrhouzlane@github.com>
2022-04-25 15:44:10 +0200

    gridSearch

--- brownie/contracts/BattleShip.sol
+++ brownie/contracts/BattleShip.sol
@@ -1,9 +1,10 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 contract starkShip {
+
     Player[2] players;
-    address[] playerAddresses;
+    address[] playerAddresses; 
     uint256 public counter;
     uint8 public currentPlayer;
     uint8 public numOfPlayers;
@@ -11,78 +12,61 @@ contract starkShip {
     uint256 locShip;
     uint256 programhash;
 
+
     struct Player {
         address addr;
-        uint256 shipPosition;
-        uint256 shifter;
+        uint shipPosition;
+        uint shifter;
         bool turn;
+        mapping(address => uint8[])) shotsHistory;
     }
 
-    enum State {
-        Initial,
-        Started,
-        Finished,
-        Winner
-    }
+
+    enum State {Initial, Started, Finished, Winner}
     State public state;
 
-    constructor(
-        uint256 shipHashLocation,
-        uint256 shifterHash,
-        uint256 programhash
-    ) {
-        Player memory playerA = Player(
-            msg.sender,
-            shipHashLocation,
-            shifterHash,
-            false
-        );
+    mapping (address => uint256[]) shotsPlayer ; //Holds the shots of a specific player ; 
+
+    // history of shots 
+    
+    mapping(address => uint8[])) positionShot2; // Holds the position of the shot in the grid for a player for a current turn. 
+
+    constructor(uint256 shipHashLocation, uint256 shifterHash, uint256 programhash) {
+        Player memory playerA = Player (msg.sender, shipHashLocation, shifterHash, false);
         players[0] = playerA;
         programhash = programhash;
         state = State.Initial;
+
     }
 
-    function playerJoins(
-        address _addr,
-        uint256 shipHashLocation,
-        uint256 shifterHash,
-        bool _turn
-    ) public {
+    function playerJoins (address _addr, uint256 shipHashLocation, uint256 shifterHash, bool _turn) public {
         require(state == State.Initial);
-        Player memory playerB = Player(
-            msg.sender,
-            shipHashLocation,
-            shifterHash,
-            true
-        );
+        Player memory playerB =  Player(msg.sender, shipHashLocation, shifterHash, true) ;
         players[1] = playerB;
         state = State.Started;
     } 
-
-    mapping(address => uint256[]) shotsPlayer; //Holds the shots of a specific player ;
-    mapping(uint256 => mapping(address => uint8[])) positionShot; // Holds the position of the shot in the grid for a player for a current turn.
-
-    //mapping (uint => mapping(address => uint8)) lastPositionShot; //Holds the index of the last fired position.
-    //mapping (uint => mapping(address => ShipPosition[])) correctPositionsHit;   //Position of the correct position -> winner
-
-    function makeShot(address _player, uint256 _shot)
-        public
-        view
-        returns (uint256[] memory)
-    {
+   
+    function makeShot(address _player, uint256 _shot) public view returns(uint256[] memory){
         require(state == State.Started);
-        return shotsPlayer[_player];
+        require(shot is not in Player.shtots)
+        positionShot[i][]
+
+        return shotsPlayer[_player] ;
     }
 
-    //Get new grid based on the last shot mapping
-    function updateGrid() public view returns (uint8) {}
+    //Get new grid based on the last shot mapping 
+    //function updateGrid() public view returns (uint256[] memory){}
 
-    //position of the last attacked position
+    //position of the last attacked position 
 
-    function getLastPositionAttacked(address _player, uint256 _counter){
-        public
-        view
-        returns (uint8)
-    }
-    
+    function getLastPositionAttacked(address _player, uint256 _counter) public view returns (uint8){}
+        
+
 }
+
+
+
+
+
+
+

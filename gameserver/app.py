db7d97cd533fddbf0e41c4c3b3821fef1635bbe2
0xSacha <sacha.graff@icloud.com>
2022-04-25 12:36:17 +0200

    add route revealer & masker program hash

--- gameserver/app.py
+++ gameserver/app.py
@@ -11,6 +11,8 @@ _ = CORS(app)
 CUR_DIR = os.getcwd()
 PATH_CAIRO_SCRIPTS_FOLDER = f"{CUR_DIR}/../cairo-scripts/"
 PATH_MOVES_JSON_FOLDER = f"{CUR_DIR}/../moves/"
+MASKER_COMPILED = "masker_compiled.json"
+REVEALER_COMPILED = "revealer_compiled.json"
 
 # TODO: should this be encapsulated in something? along with ENV variables above
 MASKER_ARGS = [
@@ -21,10 +23,44 @@ MASKER_ARGS = [
     "--print_output"
 ]
 
+MASKER_PROGRAM_HASH = [
+    "cairo-hash-program", 
+    f"--program={PATH_CAIRO_SCRIPTS_FOLDER}/{MASKER_COMPILED}"
+]
+
+REVEALER_PROGRAM_HASH = [
+    "cairo-hash-program", 
+    f"--program={PATH_CAIRO_SCRIPTS_FOLDER}/{REVEALER_COMPILED}"
+]
+
+def parse_stdout2(stdout):
+    stdout_clean = stdout.replace("\n", "")
+    return stdout_clean
+
 @app.route("/game", methods=["GET"])
 def game():
     return {
+        
+    }
 
+@app.route("/masker_program_hash", methods=["GET"])
+def masker_program_hash():
+    completed_process = subprocess.run(MASKER_PROGRAM_HASH, capture_output=True, text=True)
+    clean_stdout = parse_stdout2(completed_process.stdout)
+    return {
+        "ouput": completed_process.stdout,
+        "stderr": completed_process.stderr,
+        "clean_stdout": clean_stdout
+    }
+
+@app.route("/revealer_program_hash", methods=["GET"])
+def revealer_program_hash():
+    completed_process = subprocess.run(REVEALER_PROGRAM_HASH, capture_output=True, text=True)
+    clean_stdout = parse_stdout2(completed_process.stdout)
+    return {
+        "ouput": completed_process.stdout,
+        "stderr": completed_process.stderr,
+        "clean_stdout": clean_stdout
     }
 
 @app.route("/mask", methods=["GET"])
